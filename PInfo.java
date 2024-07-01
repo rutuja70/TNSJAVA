80057721af538e12b11effc97c2a74a2cbf583aa
rutuja70 <137986035+rutuja70@users.noreply.github.com>
2024-07-01 13:44:10 +0530

    Add files via upload

---
+++ PInfo.java
@@ -0,0 +1,12 @@
+package TNSJAVA;
+
+public class PInfo {
+public static void main(String args[]) {
+	Person person=new Person();
+	person.setName("Rutuja");
+	person.setId(101);
+	
+	System.out.println("Name:"+person.getName());
+	System.out.println("Id:"+person.getId());
+}
+}
---
+++ Person.java
@@ -0,0 +1,26 @@
+package TNSJAVA;
+
+public class Person {
+private int id;
+private String name;
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name=name;
+	
+}
+
+public int getId() {
+	return id;
+}
+
+public void setId(int id) {
+	this.id=id;
+}
+}
+
+
+
